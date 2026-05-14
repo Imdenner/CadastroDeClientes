df63a8848a5238d4b4dfadacaea9afc0aeb9b04e
Denner Fernando <denner252@hotmail.com>
2026-05-13 22:31:35 -0300

    feat: cria MissoesRepository

--- src/main/java/com/dennerdev/cadastro_clientes/Missoes/MissoesRepository.java
+++ src/main/java/com/dennerdev/cadastro_clientes/Missoes/MissoesRepository.java
@@ -1,4 +1,6 @@
 package com.dennerdev.cadastro_clientes.Missoes;
 
-public interface MissoesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 }
