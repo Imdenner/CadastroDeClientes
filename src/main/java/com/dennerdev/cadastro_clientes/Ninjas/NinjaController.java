9d8e0c1e1590b13eee40a4ca803cb97047e81081
Denner Fernando <denner252@hotmail.com>
2026-05-20 00:13:07 -0300

    feat: adiciona rota post para criar ninjas e limpa imports

--- src/main/java/com/dennerdev/cadastro_clientes/Ninjas/NinjaController.java
+++ src/main/java/com/dennerdev/cadastro_clientes/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.dennerdev.cadastro_clientes.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,37 @@ public class NinjaController {
     public String boasVindas() {
         return "Essa é minha primeira rota";
     }
+
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Essa é minha primeira rota";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping ("/todos")
+    public String mostrarNinja() {
+        return "Mostrar ninja";
+    }
+
+    // Mostrar ninja por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjasPorId() {
+        return "Mostrar ninja por ID";
+    }
+
+    // alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarPorId(){
+        return "Alterar ninja por ID";
+    }
+
+    // DeletarNinja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarPorId() {
+        return "Ninja deletado por ID";
+    }
+
+
+
 }
