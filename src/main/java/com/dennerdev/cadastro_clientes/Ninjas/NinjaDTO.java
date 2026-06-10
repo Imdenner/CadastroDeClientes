10a1aece60831a3b5e93d510b834b9b5ff5c3d2c
Denner Fernando <denner252@hotmail.com>
2026-06-09 23:06:41 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/com/dennerdev/cadastro_clientes/Ninjas/NinjaDTO.java
+++ src/main/java/com/dennerdev/cadastro_clientes/Ninjas/NinjaDTO.java
@@ -1,6 +1,20 @@
 package com.dennerdev.cadastro_clientes.Ninjas;
+import com.dennerdev.cadastro_clientes.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
-    String name;
-    int b;
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private String rank;
+    private MissoesModel missoes;
+
 }
--- src/main/java/com/dennerdev/cadastro_clientes/Ninjas/NinjaMapper.java
+++ src/main/java/com/dennerdev/cadastro_clientes/Ninjas/NinjaMapper.java
@@ -1,6 +1,33 @@
 package com.dennerdev.cadastro_clientes.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
-    int a;
-    int c;
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaModel.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaDTO.getId());
+        ninjaDTO.setNome(ninjaDTO.getNome());
+        ninjaDTO.setEmail(ninjaDTO.getEmail());
+        ninjaDTO.setIdade(ninjaDTO.getIdade());
+        ninjaDTO.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaDTO.setRank(ninjaDTO.getRank());
+        ninjaDTO.setMissoes(ninjaDTO.getMissoes());
+        return ninjaDTO;
+    }
+
+
 }
